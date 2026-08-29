a186b7600eeb31f4e6a17f4660dc3e21ccd02a85
driftbluestone <driftbluestone@gmail.com>
2026-08-29 13:58:59 -0700

    0.2.6

--- src/driftech_lib/ipc/server.py
+++ src/driftech_lib/ipc/server.py
@@ -2,7 +2,9 @@ import asyncio, logging
 logger = logging.getLogger("ipc:server")
 __all__ = ["start"]
 
-async def start(HOST, PORT):
+open_connections = 1
+
+async def start(HOST = "127.0.0.1", PORT = 8000):
     server = await asyncio.start_server(interface, HOST, PORT)
     logger.info(f"[LISTENING] Server is running on {HOST}:{PORT}")
     async with server:
@@ -10,7 +12,8 @@ async def start(HOST, PORT):
 
 async def interface(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
     addr = writer.get_extra_info("peername")
-    logger.info(f"[CONNECTION]: Connection opened for {addr}")
+    logger.info(f"[CONNECTION] Connection opened for {addr} #{open_connections}")
+    open_connections += 1
     try:
         while True:
             data = await reader.read(1024)
@@ -20,10 +23,12 @@ async def interface(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
             writer.write(b"Message processed")
             await writer.drain()  # Ensure data is flushed to the network buffer
     except (asyncio.CancelledError, ConnectionResetError, BrokenPipeError, OSError):
-        logger.info(f"[DISCONNECTED] Connection closed for {addr}")
+        open_connections -= 1
+        logger.info(f"[DISCONNECTED] Connection closed for {addr} #{open_connections-1}")
         writer.close()
         await writer.wait_closed()
     finally:
-        logger.info(f"[DISCONNECTED] Connection closed for {addr}")
+        open_connections -= 1
+        logger.info(f"[DISCONNECTED] Connection closed for {addr} #{open_connections-1}")
         writer.close()
         await writer.wait_closed()
